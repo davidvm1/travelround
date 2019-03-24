6f104199ca54cda38061465fc1e3274b5693e4b6
davidvm1 <david.vvmm1997@gmail.com>
2019-03-24 15:52:35 -0500

    Linked Start, Login Sign Up

--- Travelround/www/js/my-app.js
+++ Travelround/www/js/my-app.js
@@ -2,9 +2,11 @@
     // App root element
     root: '#app',
     // App Name
-    name: 'My App',
+    name: 'travelround',
+    // Theme
+    theme: 'md',
     // App id
-    id: 'com.myapp.test',
+    id: 'com.phonegap.travelround',
     // Enable swipe panel
     panel: {
         swipe: 'left',
@@ -12,9 +14,24 @@
     // Add default routes
     routes: [
         {
-            path: '/about/',
-            url: 'about.html',
+            path: 'index.html',
+            url: 'index.html',
         },
     ],
     // ... other parameters
+});
+
+var mainView = app.views.create('.view-main', {
+
+    routes: [
+        {
+            path: 'signup.html',
+            url: 'signup.html',
+        },
+        {
+            path: 'login.html',
+            url: 'login.html',
+        },
+    ],
+
 });
