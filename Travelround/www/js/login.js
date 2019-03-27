679bf3271d6d6d284dfbeb4d5761bfa5df10a810
davidvm1 <david.vvmm1997@gmail.com>
2019-03-26 19:51:39 -0500

    Comienzo de implementacion de firebase

---
+++ Travelround/www/js/login.js
@@ -0,0 +1,27 @@
+﻿
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        window.alert("Logged In");
+    } else {
+        
+    }
+});
+
+
+function login() {
+
+    var userEmail = document.getElementById("inputUser").value;
+    var userPass = document.getElementById("inputPsw").value;
+
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        window.alert("Error : " + errorMessage);
+        // ...
+    });
+
+    
+}
+
