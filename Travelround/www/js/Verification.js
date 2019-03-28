44503bec1be7323ee9c88123eb4b4dbb812ffa90
davidvm1 <david.vvmm1997@gmail.com>
2019-03-28 18:05:12 -0500

    Login y Signup Final

---
+++ Travelround/www/js/Verification.js
@@ -0,0 +1,19 @@
+﻿function sendVerification() {
+
+    var user = firebase.auth().currentUser;
+
+    user.sendEmailVerification().then(function () {
+        // Email sent.
+
+        window.alert("Verification sent");
+        firebase.auth().signOut();
+        window.location.href = 'index.html';
+
+    }).catch(function (error) {
+        // An error happened.
+
+        window.alert("Error: " + error.message);
+    });
+
+
+}
---
+++ Travelround/www/js/signup.js
@@ -0,0 +1,35 @@
+﻿firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        // window.alert("Logged In");
+        var user = firebase.auth().currentUser;
+
+        if (user != null) {
+
+            window.location.href = 'Verification.html';
+
+        }
+
+    } else {
+
+        // window.alert("Logged Out");
+
+    }
+});
+
+function create_account() {
+
+    var userEmail = document.getElementById("inputUser").value;
+    var userPass = document.getElementById("inputPsw").value;
+
+    firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        window.alert("Error : " + errorMessage);
+        // ...
+    });
+
+  
+
+}
