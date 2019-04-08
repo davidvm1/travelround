4a1270b946e04abe9ef90835ee7eff09eabd9e4d
davidvm1 <david.vvmm1997@gmail.com>
2019-04-07 19:33:53 -0500

    Creacion de viaje

---
+++ Travelround/www/js/CreacionViaje.js
@@ -0,0 +1,11 @@
+﻿var inputLugar = document.getElementById("inputLugar");
+var botonCrear = document.getElementById("botonCrear");
+
+function crearViaje() {
+
+    var database = firebase.database().ref();
+    var lugar = inputLugar.value;
+
+    database.push().set(lugar);
+
+}
