66110141523a79ea48109b086abd9cbdcfb6293c
davidvm1 <david.vvmm1997@gmail.com>
2019-04-29 12:25:48 -0500

    Agregacion de Ventana de Viajes

---
+++ Travelround/www/js/VentanaViajes.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+
+    var rootRef = firebase.database().ref();
+
+    rootRef.on("child_added", snap => {
+
+        $("#listaViajes").append("<a href='#' class='list - group - item'>" + snap.val() + "</a>");
+       
+
+
+    });
+
+
+});
